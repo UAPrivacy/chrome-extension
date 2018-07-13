db423fd9d6371294e4ffdfa1d6f08939985c45a1
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-07-12 22:22:06 -0700

    checkpoint

--- src/popup/components/App.jsx
+++ src/popup/components/App.jsx
@@ -1,21 +1,30 @@
 import React, { PureComponent } from "react";
 import { hot } from "react-hot-loader";
-import Clause from './Clause'
+import Clause from "./Clause";
+import axios from "axios";
 
 class App extends PureComponent {
   state = {
-    clauses = []
-  }
+    clauses: []
+  };
 
   componentDidMount() {
-
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(results => {
+      this.setState({ clause: results.data.map(post => post.body) });
+    });
   }
 
   render() {
-    const {clauses} = this.state;
+    const { clauses } = this.state;
     return (
       <div>
-{clauses}
+        {clauses.map(clause => <Clause text={clause} />)}
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          totam nam sed illum architecto libero amet a inventore similique est
+          repudiandae, sit, repellendus possimus asperiores, cum assumenda non.
+          Vero, asperiores.
+        </p>
       </div>
     );
   }
