52e176f34b8e4355db8c493b0855d195f8f992fb
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-07-13 00:27:19 -0700

    checkpoint

--- src/popup/components/App.jsx
+++ src/popup/components/App.jsx
@@ -3,19 +3,30 @@ import { hot } from "react-hot-loader";
 import Clause from "./Clause";
 import axios from "axios";
 
+const Loading = () => (
+  <div
+    className="uk-flex uk-flex-center uk-flex-middle"
+    data-uk-height-viewport
+  >
+    <span uk-spinner="ratio: 4.5" className="uk-margin-auto-vertical" />
+  </div>
+);
+
 class App extends PureComponent {
   state = {
     clauses: [],
-    isLoading: true
+    isLoading: true,
+    active: "terms"
   };
 
   componentDidMount() {
+    // TODO remove timeout
     setTimeout(() => {
       axios
         .get("https://jsonplaceholder.typicode.com/posts")
         .then(results => {
           this.setState({
-            clauses: results.data.map(post => post.body),
+            clauses: results.data.map(post => post.body).slice(0, 5),
             isLoading: false
           });
         })
@@ -25,24 +36,39 @@ class App extends PureComponent {
             isLoading: false
           });
         });
-    }, 1000);
+    }, 500);
   }
 
+  handleActive = which => {
+    this.setState({ active: which });
+  };
+
   render() {
-    const { clauses, isLoading } = this.state;
-    const renderResult = isLoading ? (
-      <div
-        class="uk-flex uk-flex-center uk-flex-middle"
-        data-uk-height-viewport
-      >
-        <span uk-spinner="ratio: 4.5" class="uk-margin-auto-vertical" />
-      </div>
+    const { clauses, isLoading, active } = this.state;
+
+    return isLoading ? (
+      <Loading />
     ) : (
-      <ul class="uk-list uk-list-divider">
-        {clauses.map(clause => <Clause text={clause} />)}
-      </ul>
+      <div className="uk-section uk-section-xsmall">
+        <div className="uk-container uk-container-small">
+          <ul className="uk-subnav uk-subnav-pill uk-flex-center">
+            <li className={active === "terms" ? "uk-active" : ""}>
+              <a onClick={() => this.handleActive("terms")}>Terms</a>
+            </li>
+            <li className={active === "privacy" ? "uk-active" : ""}>
+              <a onClick={() => this.handleActive("privacy")}>Privacy</a>
+            </li>
+          </ul>
+
+          <ul
+            className="uk-list uk-list-divider"
+            uk-scrollspy="cls: uk-animation-fade; target: > li; delay: 300; repeat: true"
+          >
+            {clauses.map(clause => <Clause text={clause} />)}
+          </ul>
+        </div>
+      </div>
     );
-    return renderResult;
   }
 }
 
