2646219be32912f283e71dc82a2bf01fa7256af6
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-07-23 14:01:50 -0700

    checkpoint

--- src/popup/components/App.jsx
+++ src/popup/components/App.jsx
@@ -1,46 +1,37 @@
-import React, { PureComponent } from "react";
-import { hot } from "react-hot-loader";
-import PropTypes from "prop-types";
-import Clause from "./Clause";
+import React, { PureComponent } from 'react';
+import { hot } from 'react-hot-loader';
+import PropTypes from 'prop-types';
+import Clause from './Clause';
 
 class App extends PureComponent {
   state = {
-    active: "terms"
+    active: 'terms',
   };
 
-  handleActive = which => {
+  handleActive = (which) => {
     this.setState({ active: which });
   };
 
   render() {
     const { active } = this.state;
     const { terms, privacies } = this.props;
-    const clauses = active === "terms" ? terms : privacies;
+    const clauses = active === 'terms' ? terms : privacies;
     return (
       <div className="uk-section uk-section-xsmall">
         <div className="uk-container uk-container-small">
           <ul className="uk-subnav uk-subnav-pill uk-flex-center">
-            <li className={active === "terms" ? "uk-active" : ""}>
-              <a
-                onClick={() => this.handleActive("terms")}
-                uk-tooltip="title:User Agreements; pos: left"
-              >
+            <li className={active === 'terms' ? 'uk-active' : ''}>
+              <button onClick={() => this.handleActive('terms')} uk-tooltip="title:User Agreements; pos: left">
                 Terms
-              </a>
+              </button>
             </li>
-            <li className={active === "privacy" ? "uk-active" : ""}>
-              <a
-                onClick={() => this.handleActive("privacy")}
-                uk-tooltip="title:Privacy Policies ; pos: right"
-              >
+            <li className={active === 'privacy' ? 'uk-active' : ''}>
+              <button onClick={() => this.handleActive('privacy')} uk-tooltip="title:Privacy Policies ; pos: right">
                 Privacy
-              </a>
+              </button>
             </li>
           </ul>
-          <ul
-            className="uk-list uk-list-divider"
-            uk-scrollspy="cls: uk-animation-fade; target: > li; delay: 300; repeat: true"
-          >
+          <ul className="uk-list uk-list-divider" uk-scrollspy="cls: uk-animation-fade; target: > li; delay: 300; repeat: true">
             {clauses.map((clause, idx) => <Clause key={idx} text={clause} />)}
           </ul>
         </div>
@@ -51,7 +42,7 @@ class App extends PureComponent {
 
 App.propTypes = {
   terms: PropTypes.array.isRequired,
-  privacies: PropTypes.array.isRequired
+  privacies: PropTypes.array.isRequired,
 };
 
 export default hot(module)(App);
--- src/popup/components/Clause.jsx
+++ src/popup/components/Clause.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Clause = ({ text }) => {
-  return <li>{text}</li>;
-};
+const Clause = ({ text }) => (
+  <li>
+    {text}
+  </li>
+);
 
 Clause.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
 };
 
 export default Clause;
