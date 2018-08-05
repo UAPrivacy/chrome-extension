d3e42f57107c43f65954c5ce2c95d023c2cb74e5
samuel.karani <samuel.karani@berkeley.edu>
2018-08-05 01:28:22 -0700

    checkpoint

--- src/api/src/urls/github.js
+++ src/api/src/urls/github.js
@@ -1,5 +1,30 @@
 import axios from 'axios';
 
+const categories = [
+  'privacies',
+  'terms',
+];
+
+function selector(urls) {
+  const pages = {};
+  categories.forEach((attr) => {
+    if (urls[attr]) {
+      pages[attr] = urls[attr][0];
+    }
+  });
+  return pages;
+}
+
+function findURLs(url, urls) {
+  const key = Object.keys(urls).find(k => url.includes(k) || k.includes(url));
+  // console.log(`key: ${key} vs url: ${url}`); // missing key detector
+  let data = {};
+  if (key) {
+    data = urls[key];
+  }
+  return data;
+}
+
 async function getURLs() {
   const url = 'https://raw.githubusercontent.com/UAPrivacy/server/master/src/routes/data/index.new.json';
   const { data, status } = await axios.get(url);
@@ -9,4 +34,4 @@ async function getURLs() {
   return data;
 }
 
-export default getURLs;
+export default name => selector(findURLs(name, getURLs()));
--- src/api/src/urls/index.js
+++ src/api/src/urls/index.js
@@ -1,29 +1,6 @@
 import getURLs from './github';
+import getLinks from './getLinks';
 
-const categories = [
-  'privacies',
-  'terms',
-];
+const getURLsActive = getURLs;
 
-function selector(urls) {
-  const pages = {};
-  categories.forEach((attr) => {
-    if (urls[attr]) {
-      pages[attr] = urls[attr][0];
-    }
-  });
-  return pages;
-}
-
-function findURLs(url, urls) {
-  const key = Object.keys(urls).find(k => url.includes(k) || k.includes(url));
-  // console.log(`key: ${key} vs url: ${url}`); // missing key detector
-  let data = {};
-  if (key) {
-    data = urls[key];
-  }
-  return data;
-}
-
-const main = name => selector(findURLs(name, getURLs()));
 export default main;
