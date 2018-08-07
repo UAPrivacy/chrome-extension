2af471f3a1d2b6b7a4841c0422e9bc0fbc2e84f6
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-08-06 23:26:29 -0700

    checkpoint

--- src/api/src/urls/github.js
+++ src/api/src/urls/github.js
@@ -1,35 +1,73 @@
 import axios from 'axios';
 
-function selector(urlsObj) {
+function selector(cacheObj) {
   const categories = ['privacies', 'terms'];
   const pages = {};
   categories.forEach(category => {
-    if (urlsObj[category]) {
-      pages[category] = [urlsObj[category]];
+    const urlsObj = cacheObj[category];
+    if (urlsObj) {
+      if (typeof urlsObj === 'string') {
+        pages[category] = urlsObj;
+      }
+
+      if (Array.isArray(urlsObj)) {
+        pages[category] = [urlsObj];
+      }
     }
   });
   return pages;
 }
 
-function findURLs(name, urlsObj) {
-  const key = Object.keys(urlsObj).find(
-    k => name.includes(k) || k.includes(name)
-  );
+const findKey = (possiblekeys, name) =>
+  possiblekeys.find(k => name.includes(k) || k.includes(name));
+
+function findURLs(name, cache) {
+  const { auto, manual } = cache;
+  let key;
+  key = findKey(Object.keys(manual), name);
+
+  if (!key) {
+    key = findKey(Object.keys(auto), name);
+  }
   // console.log(`key: ${key} vs url: ${url}`); // missing key detector
   if (!key) {
     throw Error(`${name} entry not found`);
   }
-  return urlsObj[key];
+  return cache[key];
 }
 
-async function getURLs() {
-  const url =
-    'https://raw.githubusercontent.com/UAPrivacy/server/master/src/routes/data/index.new.json';
-  const { data, status } = await axios.get(url);
+const getURL = url => axios.get(url);
+
+function checkErrorStatus(status) {
   if (status >= 400) {
     throw Error(`status: ${status}`);
   }
-  return data;
+  return true;
 }
 
-export default async name => selector(findURLs(name, await getURLs()));
+async function getCache() {
+  const autoURL =
+    'https://raw.githubusercontent.com/UAPrivacy/generators/master/src/data/cache-urls-auto.json';
+
+  const manualURL =
+    'https://raw.githubusercontent.com/UAPrivacy/generators/master/src/data/cache-urls-manual.json';
+
+  const [manual, auto] = await Promise.all([
+    getURL(autoURL),
+    getURL(manualURL)
+  ]);
+
+  const cache = {};
+
+  if (checkErrorStatus(manual.status)) {
+    cache.manual = manual.data;
+  }
+
+  if (checkErrorStatus(auto.status)) {
+    cache.auto = auto.data;
+  }
+
+  return cache;
+}
+
+export default async name => selector(findURLs(name, await getCache()));
