2f79156bfa6d3d134867daae8e4d7dc4c147ef0e
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-08-08 15:38:19 -0700

    more error checking and refactoring

--- src/api/src/index.js
+++ src/api/src/index.js
@@ -6,15 +6,14 @@ import { isEmptyObj } from '../../shared';
 async function fetchSummaries(name) {
   const results = {};
   const urlsObj = await getURLs(name);
-  if (urlsObj && !isEmptyObj(urlsObj)) {
+  if (!isEmptyObj(urlsObj)) {
     for (const [key, url] of Object.entries(urlsObj)) {
       const text = await extract(url);
       if (!text) throw Error(`${name} text not found`);
       const summaries = await summarize({
         text: text
       });
-      if (!summaries || summaries.length === 0)
-        throw Error(`${name} no summaries found`);
+      if (!summaries === 0) throw Error(`${name} no summaries found`);
       results[key] = summaries;
     }
     if (isEmptyObj(results)) {
--- src/shared/index.js
+++ src/shared/index.js
@@ -21,7 +21,7 @@ export function getCurrentURL() {
 }
 
 const isEmptyObj = obj =>
-  Object.keys(obj).length === 0 && obj.constructor === Object;
+  obj && Object.keys(obj).length === 0 && obj.constructor === Object;
 
 const emptyFunc = () => {};
 
